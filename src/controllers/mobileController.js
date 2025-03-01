763b843a8842abb5a56a155cb678a97b2bae1b89
madvin <madvin@abv.bg>
2025-03-01 12:33:20 +0200

    rey catch up on mobile mobileController

--- src/controllers/mobileController.js
+++ src/controllers/mobileController.js
@@ -1,26 +1,27 @@
-import { Router } from 'express';
-import { getDate, isAuth  } from '../middlewares/authMiddleware.js';
-import { getErrorMessage } from '../utils/errorHandler.js';
-import mobileService from '../services/mobileService.js';
+import { Router } from "express";
+import { getDate, isAuth } from "../middlewares/authMiddleware.js";
+import { getErrorMessage } from "../utils/errorHandler.js";
+import mobileService from "../services/mobileService.js";
 
 const mobileController = Router();
 
-mobileController.get('/create', isAuth, getDate, (req, res) => {
-    res.render('mobiles/create');
+mobileController.get("/create", isAuth, getDate, (req, res) => {
+  res.render("mobiles/create");
 });
 
-mobileController.post('/create', isAuth, getDate, async (req, res) => {
-    const mobileData = req.body;
-    const today = req.today;
-    console.log(today);
-    
+mobileController.post("/create", isAuth, getDate, async (req, res) => {
+  const mobileData = req.body;
+  const today = req.today;
 
-    try {
-        await mobileService.createMobile({ date: today, ...mobileData });
-        res.redirect('/');
-    } catch (err) {
-        return res.redirect('/create', { mobile: req.body, error: getErrorMessage(err) });
-    }
+  try {
+    await mobileService.createMobile({ date: today, ...mobileData });
+    res.redirect("/");
+  } catch (err) {
+    return res.redirect("/create", {
+      mobile: req.body,
+      error: getErrorMessage(err),
+    });
+  }
 });
 
 export default mobileController;
