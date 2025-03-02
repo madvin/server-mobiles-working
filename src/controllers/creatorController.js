440ed6853d68f6d1482d6c8d23e4b396be8caf79
madvin <madvin@abv.bg>
2025-03-02 17:55:05 +0200

    creatorController creators handlebars fixes

--- src/controllers/creatorController.js
+++ src/controllers/creatorController.js
@@ -1,14 +1,14 @@
-import { Router } from 'express';
-import authService from '../services/authService.js';
-import mobileService from '../services/mobileService.js';
+import { Router } from "express";
+import authService from "../services/authService.js";
+import mobileService from "../services/mobileService.js";
 
 const creator = Router();
 
-creator.get('/creators', async (req, res) => {
-    const creators = await authService.getAllUsers();
-    const mobiles = await mobileService.getAll();
-    res.render('creators', { creators, mobiles });
-}
-);
+creator.get("/creators", async (req, res) => {
+  const creators = await authService.getAllUsers();
+  const mobiles = await mobileService.getAll();
+
+  res.render("creators", { creators, mobiles });
+});
 
 export default creator;
