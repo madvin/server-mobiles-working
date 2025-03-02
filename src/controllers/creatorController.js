024e84a83eb15cb0ae176008797d448968e0f1c4
madvin <madvin@abv.bg>
2025-03-02 16:22:59 +0200

    update creator page and controller

--- src/controllers/creatorController.js
+++ src/controllers/creatorController.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import authService from '../services/authService.js';
+import mobileService from '../services/mobileService.js';
 
 const creator = Router();
 
 creator.get('/creators', async (req, res) => {
     const creators = await authService.getAllUsers();
-    res.render('creators', { creators });
+    const mobiles = await mobileService.getAll();
+    res.render('creators', { creators, mobiles });
 }
 );
 
