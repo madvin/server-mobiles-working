8cba05a87943de1162d1352c374114b6ec6ad12c
madvin <madvin@abv.bg>
2025-05-12 16:34:10 +0300

    remove seconds and add flag to clock

--- src/utils/time.js
+++ src/utils/time.js
@@ -3,7 +3,6 @@ export default function time() {
 
     const hours = String(timeNow.getHours()).padStart(2, '0');
     const minutes = String(timeNow.getMinutes()).padStart(2, '0');
-    const seconds = String(timeNow.getSeconds()).padStart(2, '0');
-
-    return `${hours}:${minutes}:${seconds}`;
+ 
+    return `${hours}:${minutes}`;
 }
