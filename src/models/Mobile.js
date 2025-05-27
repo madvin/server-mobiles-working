e4701794eb770948502f18715b7feb51b72629d6
madvin <madvin@abv.bg>
2025-05-27 10:28:54 +0000

    Escape letter "o" "O" from Schema

--- src/models/Mobile.js
+++ src/models/Mobile.js
@@ -43,6 +43,7 @@ const mobileSchema = new Schema({
     },
     partNo: {
         type: String,
+        match: [/^[^oO]*$/, 'Username cannot contain the letters "o" or "O"'],
         required: [true, "Part Number is required!"],
         minLength: [5, "Part Number should be at least 5 characters long!"],
         maxLength: [22, "Part Number should be less than 22 characters long!"],
