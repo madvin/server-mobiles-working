6b17d0765ec8d274e94e125e313e59e38925cf2d
madvin <madvin@abv.bg>
2025-08-04 08:43:59 +0000

    favicon

--- src/index.js
+++ src/index.js
@@ -39,7 +39,6 @@ app.set('view engine', 'hbs');
 app.set('views', './src/views');
 
 app.use('/static', express.static('src/public'));
-// app.use(express.static('src/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
